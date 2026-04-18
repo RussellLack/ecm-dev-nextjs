f0e1239d4ef138e9456d43b073f804406cb16be8
Russell  Lack <62655965+RussellLack@users.noreply.github.com>
2026-04-18 16:35:36 +0200

    Remove nested ecm-dev-nextjs folder

--- ecm-dev-nextjs/sanity/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import homePage from "./homePage";
-import service from "./service";
-import servicePackage from "./servicePackage";
-import caseStudy from "./caseStudy";
-import post from "./post";
-
-export const schemaTypes = [homePage, service, servicePackage, caseStudy, post];
